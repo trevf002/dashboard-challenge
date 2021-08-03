b8cb4cdbca9fd75252cc8344c705ddf20309dc06
trevf002 <72757928+trevf002@users.noreply.github.com>
2021-08-03 14:15:34 -0700

    update

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,41 @@
+var bbData;
+function init() {
+  d3.json("../data/samples.json").then((data) => {
+    bbData = data
+    var names = data.names
+    var selDataset = d3.select("#selDataset");
+    names.forEach(n=>{
+      selDataset.append("option").property("value", n).text(n)
+    })
+    buildCharts()
+})
+}
+function buildCharts() {
+  var selected = d3.select("#selDataset").property("value");
+  var metadata = bbData.metadata.filter(obj => obj.id == selected)[0];
+  var samples = bbData.samples.filter(obj => obj.id == selected)[0];
+  var demo = d3.select("#sample-metadata")
+  demo.html("")
+  Object.entries(metadata).forEach(([key,value])=>{
+    demo.append("p").html(`<b>${key}:</b> ${value}`)
+  })
+  var {otu_ids, sample_values, otu_labels} = samples
+  var barData = [{
+    x:sample_values.slice(0, 10).reverse(),
+    y:otu_ids.slice(0, 10).reverse().map(x=>`ID ${x}`),
+    text:otu_labels.slice(0, 10).reverse(),
+    type: "bar",
+    orientation: "h",
+  }]
+  var barLayout = {
+    title: "Top Ten Bacteria",
+    margin: { t: 30, l: 150 }
+  } 
+  var config = {
+    responive: true
+  }
+  Plotly.newPlot("bar", barData, barLayout, config)
+}
+
+
+init()
